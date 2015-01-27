ca2fe2b27c46fc9eab82ec51001730bbc43dc04e
Francis Deslauriers <francis.deslauriers@mail.utoronto.ca>
2015-01-26 20:26:37 -0500

    Add freezer option to launch script

--- launch-vm.sh
+++ launch-vm.sh
@@ -1,9 +1,10 @@
 echo "Lauching tcp rediction from HOST:22222 to GUEST:22 for ssh login"
 echo "e.g. ssh user@localhost -p 22222"
 qemu-system-x86_64 \
-		-smp 	cpus=2,cores=2,threads=2\
+		-machine freezer=off \
+		-smp 	cpus=1\
 		-enable-kvm  \
-		-drive file=debian_wheezy_amd64_standard.qcow2,if=ide \
+		-drive file=debian_wheezy_i386_standard.qcow2,if=ide,cache=none\
 		-m 1024 \
 		-redir 	tcp:22222::22 \
 		-rtc 	clock=vm,driftfix=none \

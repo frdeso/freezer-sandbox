cb52f12c8ad6fafa14b2fcd8f347aa275e545a04
root <root@debian-i386>
2015-02-03 04:56:39 +0000

    Update perf test scripts and commit results for regular test cases

--- tests/conf.sh
+++ tests/conf.sh
@@ -1,6 +1,6 @@
-TYPE=delta
-BASE_DIR=/home/frdeso/projets/freezer/tests/
-#BASE_DIR=/root/freezer-sandbox/tests/
+TYPE=baremetal
+#BASE_DIR=/home/frdeso/projets/freezer/tests/
+BASE_DIR=/root/freezer-sandbox/tests/
 INPUT_FILE=linux
-INPUT_PATH=/home/frdeso/projets/
-#INPUT_PATH=/root/
+#INPUT_PATH=/home/frdeso/projets/
+INPUT_PATH=/root/
--- tests/perf-find.sh
+++ tests/perf-find.sh
@@ -1,7 +1,8 @@
 #!/bin/bash -x
 CONF_FILE=conf.sh
 RESULTS_DIR=results/perf/
-source /home/frdeso/projets/freezer/tests/$CONF_FILE
+#source /home/frdeso/projets/freezer/tests/$CONF_FILE
+source /root/freezer-sandbox/tests/$CONF_FILE
 RESULTS_DIR=$BASE_DIR$RESULTS_DIR$TYPE/
 RESULTS_FILE=find.time
 RESULTS_PATH=$RESULTS_DIR$RESULTS_FILE
